d95ee79c5d79dee644f6313ac718a617553e3e88
al-martyn1 <amart@mail.ru>
2026-03-19 11:04:37 +0300

    Тест извлечения сгенерированных препромптов

--- tests/tmp/almai-md-split.bat
+++
@@ -1,2 +0,0 @@
-almai-md-join --list           ppp-res3.md
-almai-md-join --overwrite -o=. ppp-res3.md
---
+++ tests/tmp/run-almai-md-split.bat
@@ -0,0 +1,2 @@
+almai-md-split --list           ppp-res3.md
+almai-md-split --overwrite -o=. ppp-res3.md

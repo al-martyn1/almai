9b2d787d0218b29e3efac692e8a557fb8aaf069b
al-martyn1 <amart@mail.ru>
2026-04-07 16:43:40 +0300

    Протестировал переименование файла новой функцией umba::filesys::move

---
+++ _src/almai/_test07.cpp
@@ -0,0 +1,71 @@
+/*! \file
+    \brief Тестируем переименование файла
+ */
+
+#include "utils.h"
+#include "Preprompt.h"
+//
+#include "marty_cpp/src_normalization.h"
+#include "umba/filesys.h"
+#include "umba/shellapi.h"
+#include "umba/debug_helpers.h"
+
+//
+
+#include <iostream>
+
+using std::cout;
+using std::cerr;
+
+
+
+//----------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+    UMBA_USED(argc);
+    UMBA_USED(argv);
+
+    std::string oldName, newName;
+
+    if (argc>2)
+    {
+        oldName = argv[1];
+        newName = argv[2];
+    }
+
+    if (umba::isDebuggerPresent())
+    {
+        // std::string cwd;
+        // std::string rootPath = umba::shellapi::getDebugAppRootFolder(&cwd);
+        // std::cout << "App Root Path: " << rootPath << "\n";
+        // std::cout << "Working Dir  : " << cwd << "\n";
+        //  
+        // filename = rootPath + "/tests/test06";
+    }
+
+    if (oldName.empty())
+    {
+        cerr << "No ExistingFileName taken\n";
+        return 1;
+    }
+
+    if (newName.empty())
+    {
+        cerr << "No NewFileName taken\n";
+        return 1;
+    }
+
+    if (umba::filesys::move(oldName, newName))
+    {
+        cerr << "OK: " << oldName << " -> " << newName << "\n";
+    }
+    else
+    {
+        cerr << "Failed to move (rename): " << oldName << " -> " << newName << "\n";
+        return 1;
+    }
+
+    return 0;
+}
+
+

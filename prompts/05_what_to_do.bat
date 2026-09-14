f0cb75483555acf745935a9ed732e83b35b9b47d
al-martyn1 <amart@mail.ru>
2026-09-15 01:01:06 +0300

    промпты

--- prompts/05_what_to_do.bat
+++ prompts/05_what_to_do.bat
@@ -8,9 +8,10 @@
 @set PROMPTS_MD_05="%PROMPTS_DIR%\04_what_to_do_reply_patch05\__description__.md_"
 @set PROMPTS_MD_06="%PROMPTS_DIR%\04_what_to_do_reply_patch06\__description__.md_"
 
-@set PROMPTS_MDS=%PROMPTS_MD_01% %PROMPTS_MD_04% %PROMPTS_MD_05% %PROMPTS_MD_06%
+@set PROMPTS_MDS=%PROMPTS_MD_01% %PROMPTS_MD_04% 
+@rem  %PROMPTS_MD_05% %PROMPTS_MD_06%
 
-almai-md-join %CMN_OPTS% "-o=%~dp0\%~n0.md" "--hefooter=%~dp0\%~n0.txt" %CDT_SRC% %TESTS_SRCS% %TESTS_OUTPUT% %PROMPTS_MDS%
+almai-md-join %CMN_OPTS% "-o=%~dp0\%~n0.md" --attach-format=md "--hefooter=%~dp0\%~n0.txt" %CDT_SRC% %TESTS_SRCS% %TESTS_OUTPUT% %PROMPTS_MDS%
 
 
 @rem по поводу автоматического преобразования инсерта из буфера в аттач
